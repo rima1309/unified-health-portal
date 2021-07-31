d5c168102e96b3b82cc4b2a91ffdfefcfa753856
rima1309 <rima.rd.93@gmail.com>
2021-07-31 12:27:58 +0530

    add component folder

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './Footer';
+import Header from './Header';
+
+
+function App() {
+  return (  
+    <div>
+    <Header />
+    <div id="content">      
+    </div>
+    <Footer />
+  </div>
+  );
+}
+
+export default App;
